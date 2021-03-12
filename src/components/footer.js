8fa8a81780036f22e81fcf0c5a7ce08fe3f5b46e
StevRamos <stevhr@gmail.com>
2021-03-11 23:42:03 -0500

    footer added

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,43 +1,36 @@
 import React from "react";
-import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
+import SimpleReactFooter from "simple-react-footer";
 
 const FooterPage = () => {
+    const description = "We have implemented in Pytorch the XGAN model by Google based only on the original paper.";
+  const title = "XGAN implementation from scratch.";
+  const columns = [
+    {
+        title: "Resources",
+        resources: [
+            {
+                name: "Our repository",
+                link: "https://github.com/paper2code-pucp/avatar-image-generator"
+            },
+            {
+                name: "XGAN Paper",
+                link: "https://arxiv.org/abs/1711.05139"
+            }
+        ]
+    }
+ ];
   return (
-    <MDBFooter color="blue" className="font-small pt-4 mt-4">
-      <MDBContainer fluid className="text-center text-md-left">
-        <MDBRow>
-          <MDBCol md="6">
-            <h5 className="title">Footer Content</h5>
-            <p>
-              Here you can use rows and columns here to organize your footer
-              content.
-            </p>
-          </MDBCol>
-          <MDBCol md="6">
-            <h5 className="title">Links</h5>
-            <ul>
-              <li className="list-unstyled">
-                <a href="#!">Link 1</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 2</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 3</a>
-              </li>
-              <li className="list-unstyled">
-                <a href="#!">Link 4</a>
-              </li>
-            </ul>
-          </MDBCol>
-        </MDBRow>
-      </MDBContainer>
-      <div className="footer-copyright text-center py-3">
-        <MDBContainer fluid>
-          &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> MDBootstrap.com </a>
-        </MDBContainer>
-      </div>
-    </MDBFooter>
+      
+    <SimpleReactFooter 
+    description={description} 
+    title={title}
+    columns={columns}
+    iconColor="#FFFFFF"
+    backgroundColor="#0077BE"
+    fontColor="#FFFFFF"
+    copyright="uwu"
+ />
+  
   );
 }
 
