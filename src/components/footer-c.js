41755755f7d6cfb6bc03aae184267d2672047077
StevRamos <stevhr@gmail.com>
2021-03-19 17:02:45 -0500

    footer changed

--- src/components/footer-c.js
+++ src/components/footer-c.js
@@ -21,57 +21,15 @@ const Footer = () => {
         Pytorch implementation of the XGAN model based only on the original paper.
       </p>
       <Container> 
+        <Column>
+        <Heading>Resources</Heading>
+        </Column>
         <Row> 
           <Column> 
-            <Heading>About Us</Heading> 
-            <FooterLink href="#">Aim</FooterLink> 
-            <FooterLink href="#">Vision</FooterLink> 
-            <FooterLink href="#">Testimonials</FooterLink> 
+            <FooterLink href="https://github.com/paper2code-pucp/avatar-image-generator">Our repository</FooterLink> 
           </Column> 
           <Column> 
-            <Heading>Services</Heading> 
-            <FooterLink href="#">Writing</FooterLink> 
-            <FooterLink href="#">Internships</FooterLink> 
-            <FooterLink href="#">Coding</FooterLink> 
-            <FooterLink href="#">Teaching</FooterLink> 
-          </Column> 
-          <Column> 
-            <Heading>Contact Us</Heading> 
-            <FooterLink href="#">Uttar Pradesh</FooterLink> 
-            <FooterLink href="#">Ahemdabad</FooterLink> 
-            <FooterLink href="#">Indore</FooterLink> 
-            <FooterLink href="#">Mumbai</FooterLink> 
-          </Column> 
-          <Column> 
-            <Heading>Social Media</Heading> 
-            <FooterLink href="#"> 
-              <i className="fab fa-facebook-f"> 
-                <span style={{ marginLeft: "10px" }}> 
-                  Facebook 
-                </span> 
-              </i> 
-            </FooterLink> 
-            <FooterLink href="#"> 
-              <i className="fab fa-instagram"> 
-                <span style={{ marginLeft: "10px" }}> 
-                  Instagram 
-                </span> 
-              </i> 
-            </FooterLink> 
-            <FooterLink href="#"> 
-              <i className="fab fa-twitter"> 
-                <span style={{ marginLeft: "10px" }}> 
-                  Twitter 
-                </span> 
-              </i> 
-            </FooterLink> 
-            <FooterLink href="#"> 
-              <i className="fab fa-youtube"> 
-                <span style={{ marginLeft: "10px" }}> 
-                  Youtube 
-                </span> 
-              </i> 
-            </FooterLink> 
+            <FooterLink href="https://arxiv.org/abs/1711.05139">XGAN Paper</FooterLink> 
           </Column> 
         </Row> 
       </Container> 
